9d12a44f4daff306a251cafa05af9c5bd128515e
Omid Shahsavarzadeh <omidonrails@gmail.com>
2019-03-27 17:48:52 +0430

    change urllib to requests

--- alexaChecker.py
+++ alexaChecker.py
@@ -1,20 +1,7 @@
-#import requests
+import requests
 #import bs4
-from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 siteAddress = "hamyarwp.com"
 submit_url = "https://www.alexa.com/siteinfo/" + siteAddress
-uClient = uReq(submit_url)
-page_html = uClient.read()
-uClient.close()
-
-page_soup = soup(page_html, "html.parser")
-
-
-metrics = page_soup.findAll("section", {"id":"traffic-rank-content"})
-#global_rank = metrics[0].find("span", {"class":"align-vmiddle change-wrapper change-up change-r2"})
-country_rank = metrics[0].findAll("div", {"class":"rank-row"})
-
-#print("global rank = " + global_rank.text)
-print("country rank = " + country_rank)
+page = requests.get(submit_url)
