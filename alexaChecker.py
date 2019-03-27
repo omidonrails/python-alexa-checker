0e6d7f77526bbf14656a8dbbd7331b49be3968df
Omid Shahsavarzadeh <omidonrails@gmail.com>
2019-03-27 18:20:37 +0430

    add country rank and print global and country ranks

--- alexaChecker.py
+++ alexaChecker.py
@@ -1,6 +1,7 @@
 import requests
 #import bs4
 from bs4 import BeautifulSoup as soup
+import os
 
 siteAddress = "hamyarwp.com"
 submit_url = "https://www.alexa.com/siteinfo/" + siteAddress
@@ -9,5 +10,10 @@ page = requests.get(submit_url)
 
 page_content = soup(page.content , "lxml")
 
-ranks = page_content.findAll("strong")
-print(ranks)
+strong_tags = page_content.findAll("strong")
+
+global_rank = strong_tags[6].text
+print("Global Rank : " + global_rank.strip())
+
+country_rank = strong_tags[9].text
+print("Country Rank : " + country_rank.strip())
