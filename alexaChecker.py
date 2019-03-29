6973ddc464bec33bfd86c6d17b67d5ba486f31fa
Omid Shahsavarzadeh <omidonrails@gmail.com>
2019-03-29 18:56:22 +0430

    Update alexaChecker.py

--- alexaChecker.py
+++ alexaChecker.py
@@ -5,23 +5,22 @@ import sys
 from bs4 import BeautifulSoup as soup
 
 siteAddress = sys.argv[1]
-print("SiteName : " + siteAddress)
+print("SiteName : " + siteAddress + "\n")
 submit_url = "https://www.alexa.com/siteinfo/" + siteAddress
 page = requests.get(submit_url)
 
 
-page_content = soup(page.content , "lxml")
+page_content = soup(page.content, "lxml")
 
 strong_tags = page_content.find_all("strong")
-#country_name = page_content.find_all('div',
-#             {'class': lambda x: x
-#                       and 'dynamic-icon' in x.split()
-#             }
-#            )
-#
+
+country_name = page_content.find_all("table", {
+    "id": "demographics_div_country_table"})
+
+print(country_name[0].a)
 
 global_rank = strong_tags[6].text
-print("Global Rank : " + global_rank.strip())
+print("\nGlobal Rank : " + global_rank.strip())
 
 country_rank = strong_tags[9].text
 print("Country Rank : " + country_rank.strip())
